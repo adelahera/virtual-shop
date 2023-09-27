5b7be16b49fe3feec4ca96bd2e2dae92348b5fb7
adelahera <dlhlalejandro@gmail.com>
2023-09-27 21:14:22 +0200

    Falta ultima consultas y arreglar imagenes

--- e-commerce/Seed.py
+++ e-commerce/Seed.py
@@ -5,13 +5,34 @@ from pprint import pprint
 from datetime import datetime
 from typing import Any
 import requests
+import os
 
 		
 # https://requests.readthedocs.io/en/latest/
 def getProductos(api):
 	response = requests.get(api)
 	return response.json()
-				
+
+def getCompras(api):
+	response = requests.get(api)
+	return response.json()
+
+# limpia la base de datos
+def limpiarBD():
+	productos_collection.delete_many({})
+	compras_collection.delete_many({})
+
+# descargar imagenes a partir de una url 
+def downloadImage(url, filename):
+	#comprueba que filename no existe
+	if os.path.exists(filename):
+		print("El fichero ya existe")
+		return 
+	response = requests.get(url)
+	if response.status_code == 200:
+		with open(filename, 'wb') as f:
+			f.write(response.content)
+
 # Esquema de la BD
 # https://docs.pydantic.dev/latest/
 # con anotaciones de tipo https://docs.python.org/3/library/typing.html
@@ -36,66 +57,68 @@ class Compra(BaseModel):
 	date: datetime
 	productos: list	
 
-
-# dato = { 
-# 	'name': "MBJ Women's Solid Short Sleeve Boat Neck V ", 
-# 	'price': 9.85, 
-# 	'description': '95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem', 'category': "women's clothing", 
-# 	'category': "women's clothing",
-# 	'iamge': None, 
-# 	'rating': {'rate': 4.7, 'count': 130}
-# }
-
-# # Valida con el esquema:
-# # daría error si no corresponde algún tipo 
-# producto = Producto(**dato)
-
-# print(producto.descripción)
-# pprint(producto.model_dump()) # Objeto -> python dict
-
-
-# # Conexión con la BD				
-# # https://pymongo.readthedocs.io/en/stable/tutorial.html
-				
-# productos_collection.insert_one(producto.model_dump()) 
-				
-# print(productos_collection.count_documents({}))
-
-# # todos los productos
-# lista_productos_ids = []
-# for prod in productos_collection.find():
-# 	pprint(prod)
-# 	print(prod.get('_id'))   # Autoinsertado por mongo
-# 	lista_productos_ids.append(prod.get('_id'))
-	
-# print(lista_productos_ids)
-	
-# nueva_compra = {
-# 	'usuario': 'fulanito@correo.com',
-# 	'fecha': datetime.now(),
-# 	'productos': lista_productos_ids
-# }
-	
-# # valida
-# compra = Compra(**nueva_compra)
-# pprint(compra.model_dump())
-# # añade a BD
-# compras_collection = tienda_db.compras  # Colección
-# compras_collection.insert_one(compra.model_dump())
-	
-# for com in compras_collection.find():
-# 	pprint(com)
-
+# Conexión a la BD	
 client = MongoClient('mongo', 27017)
 
-tienda_db = client.tienda                	# Base de Datos
+# Directorio para las imágenes
+directorio_imgs = "/e-commerce/imagenes"
+
+# Base de Datos
+tienda_db = client.tienda                	
 productos = getProductos('https://fakestoreapi.com/products')
+compras = getCompras('https://fakestoreapi.com/carts')
 
-# Insertar todos los productos
 productos_collection = tienda_db.productos  # Colección
+compras_collection = tienda_db.compras  # Colección
+	
+limpiarBD()
 
+for prod in productos:
+	prod.pop('id')
+
+for comp in compras:
+	comp.pop('id')
+
+
+# Insertar todos los productos y compras
+productos_collection.insert_many(productos)
+compras_collection.insert_many(compras)
+
+# Descargar las imagenes de los productos en la carpeta imagenes
 for prod in productos_collection.find():
-	prod.pop('_id')
+	url = prod.get('image')
+	ruta_archivo = directorio_imgs + "/" + str(url)
+	if url is not None:
+		downloadImage(url, ruta_archivo)	
+
+# Consultas a la base de datos
+# Electrónica entre 100 y 200€, ordenados por precio
+for prod in productos_collection.find({"category":"electronics", "price": {"$gt": 100, "$lt": 200}}).sort("price"):
 	pprint(prod)
 
-productos_collection.insert_many(productos)
+# # Productos que contengan la palabra 'pocket' en la descripción
+# for prod in productos_collection.find({"description": {"$regex": ".*pocket.*"}}):
+# 	pprint(prod)
+
+# # Productos con puntuación mayor de 4
+# for prod in productos_collection.find({"rating.rate": {"$gt": 4}}):
+# 	pprint(prod)
+
+# # Productos de categoria men's clothing, ordenada por puntuación
+# for prod in productos_collection.find({"category":"men's clothing"}).sort("rating.rate"):
+# 	pprint(prod)
+
+# Facturación total de la tienda
+# total = 0
+# for comp in compras_collection.find():
+#     productos = comp.get('productos')
+#     if productos is not None and isinstance(productos, list):
+#         for prod in productos:
+#             total += prod.get('price')
+
+# print("Facturación total: " + str(total) + "€")
+
+# Facturación por categoría de producto
+# categorias = []
+# for prod in productos_collection.find():
+# 	categorias.append(prod.get('category'))
