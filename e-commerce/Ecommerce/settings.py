7ea2fc2ea7245bb9b3e594b156f3314040c99d77
adelahera <dlhlalejandro@gmail.com>
2023-11-09 19:41:50 +0100

    añadido funciones para el log in

--- e-commerce/Ecommerce/settings.py
+++ e-commerce/Ecommerce/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'accounts'
     # 'django_bootstrap5',
 ]
 
@@ -136,10 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    "./static/imagenes"
+    "./static/imagenes",
+    "./static/src"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -194,3 +196,6 @@ LOGGING = {
 			}
 		}
 	}
+
+LOGIN_REDIRECT_URL = "/shop/"
+LOGOUT_REDIRECT_URL = "/shop/"
