512262f30be5cd5ce22f6c135ff1b35b64b562c6
adelahera <dlhlalejandro@gmail.com>
2023-12-03 12:38:06 +0100

    añade valoracion dinamica a los productos

--- e-commerce/static/js/stars.js
+++ e-commerce/static/js/stars.js
@@ -1,22 +1,30 @@
 function rate(element) {
     console.log(element.title);
-    fetch(`/api/productos/${element.title}`)
-        .then(res => res.json())
-        .then(res => console.log(res.rating))
+
+    fetch(`/shop/api/productos/${element.title}`)
+        .then(res => {
+            console.log(res); // Imprime la respuesta en la consola
+            return res.json();
+        })
+        .then(res => {
+            const prod_rating = Math.floor(res.rating.rate);
+            let starsHTML = '';
+            for (let i = 0; i < 5; i++) {
+                if (i < prod_rating) {
+                    starsHTML += '<span class="fa fa-star checked"></span>';
+                } else {
+                    starsHTML += '<span class="fa fa-star"></span>';
+                }
+            }
+            element.innerHTML = starsHTML;
+        })
         .catch(error => alert(`${error}`));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Script executed!');
-    
     const span_for_stars = document.querySelectorAll('span.sp');
+
     span_for_stars.forEach(ele => {
-        ele.innerHTML = `
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-        `;
+        rate(ele);
     });
 });
