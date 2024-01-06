3353256f68671afa87db4c35885de38208c625cd
adelahera <dlhlalejandro@gmail.com>
2024-01-06 19:01:06 +0100

    actualiza despliegue de la app
    se abre en los puertos 80 y 8000 pero no carga static

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
-gunicorn Ecommerce.wsgi:application --bind 0.0.0.0:8000 
+gunicorn Ecommerce.wsgi:application --bind 0.0.0.0:8000
